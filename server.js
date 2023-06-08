3ee334a88679fe4bd325b1ae4671d80c0fe2ef37
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-06-08 17:37:25 -0600

    added and structured all necessary files for the project and started on server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Console logging npm start success
+app.listen(PORT, () => console.log('Now listening'));
