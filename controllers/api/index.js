399e548ebb63f0f160d3c19c0e0774aca0e90625
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-06-21 21:36:02 -0600

    added api routes, homeroutes, and index.js and connected them all

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const favoritesRoutes= require('./favoritesRoutes')
+
+router.use('/users', userRoutes);
+router.use('/favorites', favoritesRoutes);
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,67 @@
+// Adding Dependencies
+const router = require('express').Router();
+const fetch = require('node-fetch');
+const {getMovieResults} = require('../utils/getMovieResults');
+
+// Fetching the API using try and catch() for GET Route
+
+router.get('/', (req, res) => {
+    try {
+        getMovieResults('avengers').then(data =>{
+            console.log(data);
+            res.render('homepage', {
+                data,
+                logged_in: req.session.logged_in
+            })
+        })
+    }
+    catch (err) {
+        res.status(500).json(err);
+}
+});
+
+router.get('/movie/:keyword', (req, res) => {
+    try {
+        getMovieResults(req.params.keyword).then(data =>{
+            console.log(data);
+            res.render('homepage', {
+                data
+                logged_in: req.session.logged_in
+            })    
+        })
+    }
+    catch (err) {
+        res.status(500).json(err);
+}
+});
+
+router.get('/synapsis/:movie', async (req, res) => {
+    try {
+        // const reviewData= await Review.findAll({where:{title: req.params.title}})
+        // if(reviewData){
+        //     const reviews=reviewData.map((review)=>review.get({plain: true}))
+        // }
+        async function getSynapsis(title, year) {
+
+            const URL = `http://www.omdbapi.com/?t=${title}&y=${year}&plot=full&apikey=72db57ad`;
+
+            const response = await fetch(`${URL}`);
+            const data = await response.json();
+            console.log(data)
+            res.render('synapsis', {
+                // the data will be sent into the synapsis.handlebars
+                ...data,
+             
+                logged_in: req.session.logged_in
+            });
+        };
+        console.log(data)
+        getSynapsis(req.params.movie);
+
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
