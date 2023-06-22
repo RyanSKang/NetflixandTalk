3a741a7cf3d29ce67863d49302a66e5b05be0ef6
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-06-21 20:43:32 -0600

    fixed my server.js set up

--- server.js
+++ server.js
@@ -3,16 +3,37 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session')
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-const sequelize= require('./config/connection');
-const sequelizeStore= require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
+
+// Creating and using session object for sequelize
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+// Allows express.js to use handlebar templates
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -21,5 +42,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(routes)
+
 // Console logging npm start success
-app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
